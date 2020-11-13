0ca3da900e875eb018c15180b7077c18944b972c
Faithfulness Alamu <vaguemail369@gmail.com>
2020-11-13 04:49:54 +0100

    Add main function

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	if err := run(getRunAddr()); err != nil {
+		log.Fatalf("run: %v", err)
+	}
+}
+
+func run(addr string) error {
+	return nil
+}
+
+func getRunAddr() string {
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "8080"
+	}
+	return net.JoinHostPort("", port)
+}
